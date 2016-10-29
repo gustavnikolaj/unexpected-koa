6c06d488cf941163ad81d156cd71929ec255b02e
Gustav Nikolaj Olsen <gno@one.com>
2016-10-29 13:11:25 +0200

    working adaption from unexpected-express

--- test/unexpected-koa.js
+++ test/unexpected-koa.js
@@ -1,3 +1,4 @@
+const Koa = require('koa');
 var expect = require('unexpected');
 var unexpectedKoa = require('../');
 
@@ -11,6 +12,36 @@ describe('unexpected-koa', function () {
             name: 'unexpected-koa',
             version: /^\d+\.\d+\.\d+$/,
             installInto: expect.it('to be a function')
+        });
+    });
+
+    describe('to yield exchange', function () {
+        before(function () {
+            expect = expect
+                .clone()
+                .use(unexpectedKoa);
+        });
+
+        it('with a koa app', function () {
+            const app = new Koa();
+
+            app.use(async ctx => {
+                ctx.body = 'foobar';
+            });
+
+            return expect(app, 'to yield exchange', {
+                request: 'GET /',
+                response: 'foobar'
+            });
+        });
+
+        it('just a handler', function () {
+            return expect(async ctx => {
+                ctx.body = 'foobar';
+            }, 'to yield exchange', {
+                request: 'GET /',
+                response: 'foobar'
+            });
         });
     });
 });
